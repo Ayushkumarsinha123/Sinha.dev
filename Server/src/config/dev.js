7c258b9ea3cc0d2b83c1a445bde331d457b7e99f
Ayushkumarsinha123 <itsharshit0005@gmail.com>
2026-04-17 21:27:10 +0530

    fix: added the ||3000 fallback in dev.js

--- Server/src/config/dev.js
+++ Server/src/config/dev.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   MONGO_URL: process.env.MONGO_URL,
   TOKEN_SECRET : process.env.TOKEN_SECRET,
 };
