d5dfdfe4664d50ab77b17b2cb764b059057e9e10
Ayushkumarsinha123 <itsharshit0005@gmail.com>
2026-04-18 16:39:58 +0530

    refactor: update reel services to use centralized API instance

--- client/src/services/reelService.js
+++ client/src/services/reelService.js
@@ -1,40 +1,40 @@
-import axios from "axios";
-const API_URL = 'http://localhost:3000/api/reels';
+
+import API from "../utils/api";
+
 // fetch reel for customer
 export const fetchReels = async () => {
-  const res = await axios.get(API_URL)
+  const res = await API.get(`/reels`);
   return res.data;
-}
+};
 
 // fetch restro own reels 
-
-export const fetchMyReels= async(token) => {
-  const response = await axios.get(`${API_URL}/my-reels`,{
-    headers : {
-      Authorization : `Bearer ${token}`
+export const fetchMyReels = async(token) => {
+  const response = await API.get(`/reels/my-reels`, {
+    headers: {
+      Authorization: `Bearer ${token}`
     }
-  })
-  return response.data;
-}
-
-// delete reel 
-
-export const deleteReel = async (id , token) => {
-  const response = await axios.delete(`${API_URL}/${id}`, {
-    headers : {Authorization : `Bearer ${token}`}
   });
   return response.data;
 };
-// update reel
-export const updateReel = async (id, updatedData, token) => {
-  const response = await axios.put(`${API_URL}/${id}`,updatedData, {
-    headers : {Authorization : `Bearer ${token}`}
+
+// delete reel 
+export const deleteReel = async (id, token) => {
+  const response = await API.delete(`/reels/${id}`, {
+    headers: { Authorization: `Bearer ${token}` }
   });
   return response.data;
-}
+};
+
+// update reel
+export const updateReel = async (id, updatedData, token) => {
+  const response = await API.put(`/reels/${id}`, updatedData, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return response.data;
+};
 
 // search reel
 export const fetchSearchResults = async (searchTerm) => {
-  const response = await axios.get(`${API_URL}/search?query=${searchTerm}`);
+  const response = await API.get(`/reels/search?query=${searchTerm}`);
   return response.data;
 };
